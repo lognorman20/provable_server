40617ebcb6875d1420609a2f8f075706f597d651
ArnaudBD <arnaud.bijuduval@gmail.com>
2024-07-10 19:00:13 +0200

    fix

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,4 @@
 import {Mainv0} from "../components/mainv0";
-import { Account, RpcProvider, json, Contract, hash} from 'starknet';
-import fs from 'fs';
-import * as dotenv from 'dotenv';
-import { initializeContract } from '../lib/contract';
 
 export default function Home() {
   return (
